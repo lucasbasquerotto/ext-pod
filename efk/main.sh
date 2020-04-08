24e144bb91502988eacdf355eb7010258de2fd73
Lucas Basquerotto <lucasbasquerotto@example.com>
2020-04-08 19:43:54 -0300

    [dev] efk

--- efk/main.sh
+++ efk/main.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-# shellcheck disable=SC1090,SC2154
+# shellcheck disable=SC1090,SC2154,SC2153
 set -eou pipefail
+
+pod_vars_dir="$POD_VARS_DIR"
+pod_layer_dir="$POD_LAYER_DIR"
 
 . "${pod_vars_dir}/vars.sh"
 
 pod_env_shared_file="$pod_layer_dir/main/log/scripts/efk.sh"
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+function error {
+	msg="$(date '+%F %T') - ${BASH_SOURCE[0]}: line ${BASH_LINENO[0]}: ${1:-}"
+	>&2 echo -e "${RED}${msg}${NC}"
+	exit 2
+}
+
 command="${1:-}"
+
+if [ -z "$command" ]; then
+	error "No command entered (env)."
+fi
+
+shift;
 
 case "$command" in
   *)
