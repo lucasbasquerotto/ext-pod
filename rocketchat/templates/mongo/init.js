19f77787c80dee6abc34792cab66adee3bb97f01
lucas99freelas <lucas99freelas@99freelas.com>
2020-06-01 16:55:08 -0300

    [dev] rocketchat

--- rocketchat/templates/mongo/init.js
+++ rocketchat/templates/mongo/init.js
@@ -1,33 +1,23 @@
 db.createUser( {
     user: "root",
-    pwd: "{{ root_password }}",
-    roles: [ { role: "root", db: "admin" } ]
+    pwd: "{{ params.root_password }}",
+    roles: [ "root" ]
 });
-
-// db.createUser({
-//     user:"root",
-//     pwd: "{{ root_password }}",
-//     roles:[
-//         {role:"dbAdmin", db:"admin"},
-//         {role:"readWriteAnyDatabase", db:"admin"},
-//         {role:"backup", db:"admin"},
-//         {role:"restore", db:"admin"}]
-// });
 
 db.createUser( {
     user: "viewer",
-    pwd: "{{ viewer_password }}",
-    roles: [ { role: "read", db: "rocketchat" } ]
+    pwd: "{{ params.viewer_password }}",
+    roles: [ "readAnyDatabase" ]
 });
 
 db.createUser({
     user: "oploguser",
-    pwd: "{{ oploguser_password }}",
+    pwd: "{{ params.oploguser_password }}",
     roles: [{role: "read", db: "local"}]
 });
 
 db.createUser({
-    user: "rocket",
-    pwd: "{{ rocket_password }}",
-    roles: [{role: "readWrite", db: "rocketchat"}]
+    user: "{{ params.db_user }}",
+    pwd: "{{ params.db_password }}",
+    roles: [{role: "readWrite", db: "{{ params.db_name }}"}]
 });
