0093d167743e6df5fa1ac4246051130e8f455382
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-03-25 23:40:03 -0300

    [dev] rocketchat - uploads - s3

--- mediawiki/scripts/local.sh
+++ mediawiki/scripts/local.sh
@@ -31,10 +31,18 @@ case "$command" in
 		"$pod_script_env_file" "local:clear-all"
 		;;
 	"clear-remote")
-		"$pod_script_env_file" "s3:subtask:s3_uploads" --s3_cmd=rb
-		"$pod_script_env_file" "s3:subtask:s3_backup" --s3_cmd=rb
-		"$pod_script_env_file" "s3:subtask:s3_uploads_replica" --s3_cmd=rb
-		"$pod_script_env_file" "s3:subtask:s3_backup_replica" --s3_cmd=rb
+		if [ "${var_custom__use_s3:-}" = 'true' ]; then
+			"$pod_script_env_file" "s3:subtask:s3_uploads" --s3_cmd=rb
+			"$pod_script_env_file" "s3:subtask:s3_backup" --s3_cmd=rb
+
+			if [ "${var_run__enable__uploads_replica:-}" = 'true' ]; then
+				"$pod_script_env_file" "s3:subtask:s3_uploads_replica" --s3_cmd=rb
+			fi
+
+			if [ "${var_run__enable__backup_replica:-}" = 'true' ]; then
+				"$pod_script_env_file" "s3:subtask:s3_backup_replica" --s3_cmd=rb
+			fi
+		fi
 		;;
 	*)
 		"$pod_env_shared_file" "$command" "$@"
