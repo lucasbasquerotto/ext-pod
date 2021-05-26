419b72ad8af07f6c6197763a806588aa20978be1
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-05-26 20:34:23 +0000

    [dev] mattermost

---
+++ mattermost/scripts/local.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# shellcheck disable=SC2154
+set -eou pipefail
+
+pod_script_env_file="$var_pod_script"
+pod_env_shared_file="$var_run__general__script_dir/shared.sh"
+
+function info {
+	"$pod_env_shared_file" "util:info" --info="${*}"
+}
+
+function error {
+	"$pod_env_shared_file" "util:error" --error="${BASH_SOURCE[0]}:${BASH_LINENO[0]}: ${*}"
+}
+
+[ "${var_run__meta__no_stacktrace:-}" != 'true' ] \
+	&& trap 'echo "[error] ${BASH_SOURCE[0]}:$LINENO"; exit $LINENO;' ERR
+
+command="${1:-}"
+
+if [ -z "$command" ]; then
+	error "No command entered (env)."
+fi
+
+shift;
+
+case "$command" in
+	"clear")
+		"$pod_script_env_file" "local:clear"
+		;;
+	"clear-all")
+		"$pod_script_env_file" "local:clear-all"
+		;;
+	"clear-remote")
+		if [ "${var_custom__use_s3:-}" = 'true' ]; then
+			"$pod_script_env_file" "s3:subtask:s3_uploads" --s3_cmd=rb
+			"$pod_script_env_file" "s3:subtask:s3_backup" --s3_cmd=rb
+
+			if [ "${var_run__enable__uploads_replica:-}" = 'true' ]; then
+				"$pod_script_env_file" "s3:subtask:s3_uploads_replica" --s3_cmd=rb
+			fi
+
+			if [ "${var_run__enable__backup_replica:-}" = 'true' ]; then
+				"$pod_script_env_file" "s3:subtask:s3_backup_replica" --s3_cmd=rb
+			fi
+		fi
+		;;
+	*)
+		"$pod_env_shared_file" "$command" "$@"
+		;;
+esac
---
+++ mattermost/scripts/shared.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# shellcheck disable=SC2154
+set -eou pipefail
+
+pod_layer_dir="$var_pod_layer_dir"
+pod_script_env_file="$var_pod_script"
+
+function info {
+	"$pod_script_env_file" "util:info" --info="${*}"
+}
+
+function error {
+	"$pod_script_env_file" "util:error" --error="${BASH_SOURCE[0]}:${BASH_LINENO[0]}: ${*}"
+}
+
+[ "${var_run__meta__no_stacktrace:-}" != 'true' ] \
+	&& trap 'echo "[error] ${BASH_SOURCE[0]}:$LINENO"; exit $LINENO;' ERR
+
+command="${1:-}"
+
+if [ -z "$command" ]; then
+	error "No command entered (env)."
+fi
+
+shift;
+
+args=("$@")
+
+# shellcheck disable=SC2214
+while getopts ':-:' OPT; do
+	if [ "$OPT" = "-" ]; then     # long option: reformulate OPT and OPTARG
+		OPT="${OPTARG%%=*}"       # extract long option name
+		OPTARG="${OPTARG#$OPT}"   # extract long option argument (may be empty)
+		OPTARG="${OPTARG#=}"      # if long option argument, remove assigning `=`
+	fi
+	case "$OPT" in
+		days_ago ) arg_days_ago="${OPTARG:-}";;
+		max_amount ) arg_max_amount="${OPTARG:-}";;
+		??* ) ;; ## ignore
+		\? )  ;; ## ignore
+	esac
+done
+shift $((OPTIND-1))
+
+pod_shared_run_file="$pod_layer_dir/shared/scripts/main.sh"
+
+case "$command" in
+	"custom:unique:log")
+		opts=()
+		opts+=( 'log_register.memory_overview' )
+		opts+=( 'log_register.memory_details' )
+		opts+=( 'log_register.entropy' )
+
+		if [ "${var_custom__use_nginx:-}" = "true" ]; then
+			opts+=( 'log_register.nginx_basic_status' )
+		fi
+
+		"$pod_script_env_file" "unique:all" "${opts[@]}"
+		;;
+	"action:exec:log_summary")
+		days_ago="${var_custom__log_summary__days_ago:-}"
+		days_ago="${arg_days_ago:-$days_ago}"
+
+		max_amount="${var_custom__log_summary__max_amount:-}"
+		max_amount="${arg_max_amount:-$max_amount}"
+		max_amount="${max_amount:-100}"
+
+		"$pod_script_env_file" "shared:log:memory_overview:summary" --days_ago="$days_ago" --max_amount="$max_amount"
+		"$pod_script_env_file" "shared:log:entropy:summary" --days_ago="$days_ago" --max_amount="$max_amount"
+
+		if [ "$var_custom__pod_type" = "app" ] || [ "$var_custom__pod_type" = "web" ]; then
+			if [ "${var_custom__use_nginx:-}" = "true" ]; then
+				"$pod_script_env_file" "shared:log:nginx:summary" --days_ago="$days_ago" --max_amount="$max_amount"
+				"$pod_script_env_file" "shared:log:nginx:summary:connections" --days_ago="$days_ago" --max_amount="$max_amount"
+			fi
+
+			if [ "${var_custom__use_haproxy:-}" = "true" ]; then
+				"$pod_script_env_file" "shared:log:haproxy:summary" --days_ago="$days_ago" --max_amount="$max_amount"
+			fi
+		fi
+
+		if [ "$var_custom__pod_type" = "app" ] || [ "$var_custom__pod_type" = "db" ]; then
+			"$pod_script_env_file" "shared:log:mysql_slow:summary" --days_ago="$days_ago" --max_amount="$max_amount"
+		fi
+
+		"$pod_script_env_file" "shared:log:file_descriptors:summary" --max_amount="$max_amount"
+		"$pod_script_env_file" "shared:log:disk:summary" \
+			--verify_size_docker_dir="${var_custom__log_summary__verify_size_docker_dir:-}" \
+			--verify_size_containers="${var_custom__log_summary__verify_size_containers:-}"
+		;;
+	"shared:action:"*)
+		action="${command#shared:action:}"
+
+		case "$action" in
+			"backup"|\
+			"block_ips"|\
+			"local.backup"|\
+			"log_register.entropy"|\
+			"log_register.memory_details"|\
+			"log_register.memory_overview"|\
+			"log_register.nginx_basic_status"|\
+			"log_summary"|\
+			"logrotate"|\
+			"nginx_reload"|\
+			"pending"|\
+			"replicate_s3"|\
+			"watch")
+				"$pod_shared_run_file" "$command" ${args[@]+"${args[@]}"}
+				;;
+			*)
+				error "$command: unsupported action: $action"
+				;;
+		esac
+		;;
+	*)
+		"$pod_shared_run_file" "$command" ${args[@]+"${args[@]}"}
+		;;
+esac
