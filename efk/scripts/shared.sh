437c2475f35a9296c7611a0da6d0cb04475dcc8a
Lucas Basquerotto <lucasbasquerotto@gmail.com>
2021-06-28 23:43:22 -0300

    [fix] exit error code (lineno: 256)

--- efk/scripts/shared.sh
+++ efk/scripts/shared.sh
@@ -17,7 +17,7 @@ function error {
 }
 
 [ "${var_run__meta__no_stacktrace:-}" != 'true' ] \
-	&& trap 'echo "[error] ${BASH_SOURCE[0]}:$LINENO" >&2; exit $LINENO;' ERR
+	&& trap 'echo "[error] ${BASH_SOURCE[0]}:$LINENO" >&2; exit 3;' ERR
 
 command="${1:-}"
 
